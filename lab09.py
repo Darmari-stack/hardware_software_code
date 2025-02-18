368cd4585d4ca3507178c8f9ee5b89ded17a8ded
Darmari-stack <alexandermari495@gmail.com>
2025-02-18 13:18:42 -0500

    project 1

---
+++ lab09.py
@@ -0,0 +1,14 @@
+def print_lyrics():
+  print ("I'm a programmer, and I'm okay.")
+  print ("I code all night, and I code all day.")
+def repeat_lyrics(count = 1):
+  for number in range(count):
+      print("{}".format(number))
+      print_lyrics()
+  print ("Done Repeating!!")
+  print("{}############".format(number),end=".")
+def main():
+  repeat_lyrics()
+  repeat_lyrics(4)
+if __name__ == "__main__":
+   main()
---
+++ lab10.py
@@ -0,0 +1,8 @@
+ def main():
+    print("This program adds two numbers.")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    total = num1 + num2
+    print("{} + {} = {}".format(num1, num2, total))
+if __name__ == "__main__":
+    main()
